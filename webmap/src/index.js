96e51b25891bdc3d2691ebb9cb21cb6864fade93
vnx-horishita <horishita@valuenex.com>
2024-03-05 15:57:42 +0900

    added text-box for metadata

--- webmap/src/index.js
+++ webmap/src/index.js
@@ -4,25 +4,27 @@ import 'maplibre-gl/dist/maplibre-gl.css';
 import './style.css';
 
 const categoryNames = ["CounterDisaster","SolarCell","AsahiKasei","DNP","Kajima","NEC","Nintendo","NipponPaper","Nissan","Omron","Sharp","TORAY","Toshiba"];
+const categoryAlias = ["防災関連技術","太陽電池技術","旭化成グループ","大日本印刷","鹿島建設","NEC","任天堂","日本製紙","日産自動車","オムロン","シャープ","東レ","東芝"];
 const slidetypes = ["経年積上","単年ごと"];
 let target_slidetype = 0;
 let flag_slidetype = "経年積上";
-const colorNames = ["Standard","Glow","Heatmap"];
+const colorNames = ["Standard","Blue","Red","Purple","Yellow"];
 const colors_chart = ['#ff6347', '#87cefa'];
 const colors_heat1 = ['interpolate',['linear'],['heatmap-density'],0,'transparent', 0.3,'transparent', 0.32,'#004263', 0.38,'#004263', 0.45,'#00596f', 0.48,'#00596f', 0.55, '#6b6b6b', 0.68, '#6b6b6b', 0.75, '#e4b67c', 0.88, '#e4b67c', 0.95, '#ffffe3'];
-const colors_heat2 = ['interpolate',['linear'],['heatmap-density'],0,'transparent', 0.25, '#00336f', 0.4, '#00596f', 0.5, '#6b6b6b', 0.8, '#e4b67c', 1, '#ffffe3'];
-const colors_heat3 = ['interpolate',['linear'],['heatmap-density'],0,'transparent', 0.25, '#08519c', 0.4, '#abdda4', 0.5, '#ffffbf', 0.8, '#fdae61', 1, '#d7191c'];
-//const colors_heat2 = ['interpolate',['linear'],['heatmap-density'],0,'transparent', 0.25, '#0078bd', 0.4, '#00a0d1', 0.5, '#79cdd8', 0.8, '#b4c78f', 1, '#ff4500'];
-const colors_option = [colors_heat1, colors_heat2, colors_heat3];
+const colors_heat2 = ['interpolate',['linear'],['heatmap-density'],0,'transparent', 0.3,'transparent', 0.32,'#08306b', 0.38,'#08306b', 0.45,'#4292c6', 0.48,'#4292c6', 0.55, '#6baed6', 0.68, '#6baed6', 0.75, '#deebf7', 0.88, '#deebf7', 0.95, '#f7fbff'];
+const colors_heat3 = ['interpolate',['linear'],['heatmap-density'],0,'transparent', 0.3,'transparent', 0.32,'#2b83ba', 0.38,'#2b83ba', 0.45,'#abdda4', 0.48,'#abdda4', 0.55, '#ffffbf', 0.68, '#ffffbf', 0.75, '#fdae61', 0.88, '#fdae61', 0.95, '#d7191c'];
+const colors_heat4 = ['interpolate',['linear'],['heatmap-density'],0,'transparent', 0.3,'transparent', 0.32,'#090720', 0.38,'#090720', 0.45,'#912b81', 0.48,'#912b81', 0.55, '#c23b75', 0.68, '#c23b75', 0.75, '#fec68a', 0.88, '#fec68a', 0.95, '#fcfdbf'];
+const colors_heat5 = ['interpolate',['linear'],['heatmap-density'],0,'transparent', 0.3,'transparent', 0.32,'#481769', 0.38,'#481769', 0.45,'#297b8e', 0.48,'#297b8e', 0.55, '#1f978b', 0.68, '#1f978b', 0.75, '#b0dd2f', 0.88, '#b0dd2f', 0.95, '#ffe843'];
+const colors_option = [colors_heat1, colors_heat2, colors_heat3, colors_heat4, colors_heat5];
 let target_colortheme = 0;
 const target_Name = (location.hash ? location.hash.slice(1) : categoryNames[0]);
 const listingPOl = document.getElementById('feature-list');
-let target_year = view_param.year2 - view_param.year1 - 1;
+let target_year = map_param.viewset.year2 - map_param.viewset.year1 - 1;
 const selected_range = document.getElementById('slider');
 const label_name = document.getElementById('nLabel');
-label_name.textContent = view_param.name;
+label_name.textContent = map_param.metadata.name;
 const label_range = document.getElementById('sLabel');
-label_range.textContent = view_param.year1 + '年～' + view_param.year2 + '年';
+label_range.textContent = map_param.viewset.year1 + '年～' + map_param.viewset.year2 + '年';
 
 const playButton = document.getElementById('playButton');
 const stopButton = document.getElementById('stopButton');
@@ -64,7 +66,7 @@ stopButton.addEventListener('click', function() {
 // ラベルを更新する関数
 function updateLabel(value) {
     const label = document.getElementById('sLabel');
-    label.textContent = view_param.year1 + '年～' + value + '年';
+    label.textContent = map_param.viewset.year1 + '年～' + value + '年';
 }
 
 const categoryLength = categoryNames.length;
@@ -73,7 +75,7 @@ for (let i = 0; i < categoryLength; i++) {
     const optionName = document.createElement('option');
     //optionName.value = categoryNames[i];
     optionName.value = '#' + categoryNames[i];
-    optionName.textContent = categoryNames[i];
+    optionName.textContent = categoryAlias[i];
     if (categoryNames[i] === target_Name) {
         optionName.selected = true;
     }
@@ -105,7 +107,7 @@ for (let i = 0; i < slidetypeLength; i++) {
 }
 
 const init_coord = [0, 0];
-const init_zoom = view_param.initZoom - 1;
+const init_zoom = map_param.viewset.initZoom - 1;
 const init_bearing = 0;
 const init_pitch = 0;
 
@@ -166,7 +168,7 @@ map.on('load', function () {
         "type": 'vector',
         "url": 'pmtiles://app/data/' + target_Name + '/doc_simple.pmtiles',
         "minzoom": 4,
-        "maxzoom": view_param.maxZoom1,//6
+        "maxzoom": map_param.viewset.maxZoom1,//6
         "cluster": true,
         "clusterMaxZoom": 4,
         "clusterRadius": 2
@@ -175,7 +177,7 @@ map.on('load', function () {
         'type': 'vector',
         "url": 'pmtiles://app/data/' + target_Name + '/doc_text.pmtiles',
         "minzoom": 4,
-        "maxzoom": view_param.maxZoom2,//9 11
+        "maxzoom": map_param.viewset.maxZoom2,//9 11
     });
     map.addSource('area_label', {
         'type': 'geojson',
@@ -233,7 +235,7 @@ map.on('load', function () {
             'heatmap-intensity': ['interpolate',['linear'],['zoom'],4,0.1,7,0.5,12,1],//4,0.1,12,1
             //'heatmap-color': ['interpolate',['linear'],['heatmap-density'],0,'transparent', 0.25, '#00336f', 0.5, '#7d7c78', 0.75, '#D9CD91', 1, '#FFFD7A'],
             'heatmap-color': colors_heat1,
-            'heatmap-radius': ['interpolate',['linear'],['zoom'],4,view_param.heatRadius1,7,view_param.heatRadius2,12,view_param.heatRadius3],
+            'heatmap-radius': ['interpolate',['linear'],['zoom'],4,map_param.viewset.heatRadius1,7,map_param.viewset.heatRadius2,12,map_param.viewset.heatRadius3],
             'heatmap-opacity': ['interpolate',['linear'],['zoom'],4,1,12,0.9]
         },
     });
@@ -243,7 +245,7 @@ map.on('load', function () {
         'type': 'symbol',
         'source': 'area_label',
         'maxzoom':8.5,//9.5
-        'filter': ['<=', view_param.labelSize1, ["get", "size1"]],
+        'filter': ['<=', map_param.viewset.labelSize1, ["get", "size1"]],
         'layout': {
             'icon-image': '',
             'visibility': 'visible',
@@ -285,7 +287,7 @@ map.on('load', function () {
         'id': 'label_pseudo',//円グラフ生成用の擬似的なレイヤ
         'source': 'area_label',
         'type': 'circle',
-        //'filter': ['<=', view_param.labelSize1, ["get", "size1"]],
+        //'filter': ['<=', map_param.viewset.labelSize1, ["get", "size1"]],
         "minzoom": 4,
         "maxzoom": 15,
         'layout': {
@@ -324,7 +326,7 @@ map.on('load', function () {
         target_year = Number(selected_range.value);
         if (flag_slidetype === "経年積上") {
             this.style.background = "#777";
-            label_range.textContent = view_param.year1 + '年～' + selected_range.value + '年';
+            label_range.textContent = map_param.viewset.year1 + '年～' + selected_range.value + '年';
             map.setFilter('doc_heat', ['>=', target_year, ["get", "year"]]);
             map.setFilter('doc_info', ['>=', target_year, ["get", "year"]]);
             map.setFilter('doc_point', ['>=', target_year, ["get", "year"]]);
@@ -353,7 +355,7 @@ function updateMarkers() {
     const targetZoom = map.getZoom();
     let targetArea;
     if (targetZoom <= 8.5) {
-        targetArea = ['<=', view_param.labelSize1, ["get", "size1"]]
+        targetArea = ['<=', map_param.viewset.labelSize1, ["get", "size1"]]
     } else {
         targetArea = ['<=', 0, ["get", "size1"]]
     };
@@ -384,6 +386,14 @@ function updateMarkers() {
     }
     markersOnScreen = newMarkers;
 }
+
+const tx_meta = map_param.metadata;
+const bx_info = document.getElementById('info-box')
+let infoContent = '';
+infoContent += '<h3>'+tx_meta.name+(tx_meta.source.startsWith("Patent") ? " 技術俯瞰図" : " 俯瞰図")+'</h3>';
+infoContent += '<p>母集団件数：'+Number(tx_meta.count).toLocaleString() +'件</p>';
+infoContent += '<p>データ取得条件：'+tx_meta.searchquery.region+'で'+tx_meta.searchquery.yearRange+'に'+tx_meta.searchquery.yearType+'された'+tx_meta.searchquery.type+'のうち、'+tx_meta.searchquery.queryTarget+'に「'+tx_meta.searchquery.queryWord+'」を含むものを、VALUENEX独自のデータベースから抽出して可視化。</p>';
+bx_info.innerHTML = infoContent;
 
 //Create a box for display setting 
 const bx_setting = document.getElementById('bx-setting')
@@ -418,7 +428,7 @@ let hp01;
 document.getElementById('slider1').addEventListener('input', function() {
         document.getElementById('slider1Value').textContent = this.value;
         hp01 = Number(this.value)/10;
-        map.setPaintProperty('doc_heat', 'heatmap-radius', ['interpolate',['linear'],['zoom'],4,view_param.heatRadius1*hp01,7,view_param.heatRadius2*hp01,12,view_param.heatRadius3*hp01]);
+        map.setPaintProperty('doc_heat', 'heatmap-radius', ['interpolate',['linear'],['zoom'],4,map_param.viewset.heatRadius1*hp01,7,map_param.viewset.heatRadius2*hp01,12,map_param.viewset.heatRadius3*hp01]);
 });
 
 //Create a legend based on the displayed layer 
@@ -583,10 +593,14 @@ document.getElementById('chartButton').style.color = "white";
 document.getElementById('settingButton').style.backgroundColor = "#999";
 document.getElementById('settingButton').style.color = "white";
 
+document.getElementById('infoButton').style.backgroundColor = "#999";
+document.getElementById('infoButton').style.color = "white";
+
 document.getElementById('feature-list').style.display ="block";
 
 document.getElementById('ta-legend').style.display ="none";
 document.getElementById('map-setting').style.display ="none";
+document.getElementById('info-box').style.display ="none";
 
 document.getElementById('returnButton').addEventListener('click', function () {
     map.flyTo({center:init_coord, zoom:init_zoom+1, bearing:init_bearing, pitch:init_pitch, speed:0.5});
@@ -637,6 +651,20 @@ document.getElementById('chartButton').addEventListener('click', function () {
 
 document.getElementById('settingButton').addEventListener('click', function () {
     const visibility = document.getElementById('map-setting');
+    if (visibility.style.display === 'block') {
+        visibility.style.display = 'none';
+        this.style.backgroundColor = "#999";
+        this.style.color = "white"
+    }
+    else {
+        visibility.style.display = 'block';
+        this.style.backgroundColor = "white";
+        this.style.color = "black";
+    }
+});
+
+document.getElementById('infoButton').addEventListener('click', function () {
+    const visibility = document.getElementById('info-box');
     if (visibility.style.display === 'block') {
         visibility.style.display = 'none';
         this.style.backgroundColor = "#999";
